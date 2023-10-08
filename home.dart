cc3ec5a271b59b37b7ee03b06aeddb84ab1e9307
Letícia Fernandes <89033765+Leeprogramer@users.noreply.github.com>
2023-10-07 22:07:18 -0300

    Create home.dart

---
+++ home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutternim/home/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [Text("FlutterNim"), Text("Leticia Silva Fernandes "), Text("RA: 1431432312025")],
+          ),
+          backgroundColor: Colors.deepOrange,
+        ),
+        body: const SingleChildScrollView(
+          child: Home(),
+        ),
+      ),
+    );
+  }
+}
